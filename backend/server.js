f4cefe7a1f221a21ef0c57e838e19b039a5c7e60
Ben83000 <benjaminboufflers1@gmail.com>
2024-08-19 00:51:25 +0100

    déploiement final

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ import cookieParser from 'cookie-parser';
 import { Server as socketIo } from 'socket.io';
 import http from 'http';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Routes importées
+// Routes
 import userRoutes from './routes/user.routes.js';
 import productRoutes from './routes/product.routes.js';
 import cartRoutes from './routes/cart.routes.js';
@@ -15,6 +17,12 @@ import orderRoutes from './routes/order.routes.js';
 
 // Chargement des variables d'environnement
 dotenv.config();
+
+// Ces lignes permettent de recréer `__dirname` et `__filename`
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectRoot = path.resolve(__dirname, '..');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,12 +33,12 @@ const server = http.createServer(app);
 // Initialise Socket.IO avec le serveur HTTP
 const io = new socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:4173',
+    methods: ['GET', 'POST'],
     credentials: true,
   },
 });
- 
+
 // Connexion à MongoDB
 const connectDB = async () => {
   try {
@@ -49,10 +57,11 @@ const connectDB = async () => {
 
     // se connecte et Surveille la collection 'orders' dans mongodb
     const orderCollection = mongoose.connection.collection('orders');
-    const changeStream = orderCollection.watch(); 
+    const changeStream = orderCollection.watch();
 
     // dés qu'un changement est detecté dans la collection orders..
-    changeStream.on('change', (change) => { // "change" est le changement effectué (par ex pour une insertion, cest le nouveau doc)
+    changeStream.on('change', (change) => {
+      // "change" est le changement effectué (par ex pour une insertion, cest le nouveau doc)
       if (change.operationType === 'insert') {
         const newOrder = change.fullDocument; // on recup le nouveau doc
         // on diffuse la nouvelle commande à tous les clients Socket.IO
@@ -68,8 +77,8 @@ const connectDB = async () => {
 // Middlewares express
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "PUT", "DELETE", "POST", "PATCH"],
+    origin: 'http://localhost:4173',
+    methods: ['GET', 'PUT', 'DELETE', 'POST', 'PATCH'],
     credentials: true,
   })
 );
@@ -78,11 +87,24 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // Configurer les routes
-app.use("/user", userRoutes); 
-app.use("/product", productRoutes);
-app.use("/cart", cartRoutes);
-app.use("/stripe", stripeRoutes);
-app.use("/order", orderRoutes);
+app.use('/user', userRoutes);
+app.use('/product', productRoutes);
+app.use('/cart', cartRoutes);
+app.use('/stripe', stripeRoutes);
+app.use('/order', orderRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  console.log("la")
+  app.use(express.static(path.join(projectRoot, 'frontend', 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(projectRoot, 'frontend', 'dist', 'index.html'));
+  });
+} else {
+  console.log("ici")
+  app.get("/", (req, res) => {
+    res.send("Api running..")
+  })
+}
 
 // Démarre le serveur HTTP et Socket.IO sur le port
 server.listen(port, () => {
