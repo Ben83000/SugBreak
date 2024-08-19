200e3c67af89c6243d514b77d427a321728e6723
Ben83000 <benjaminboufflers1@gmail.com>
2024-08-19 12:46:57 +0100

    fix

--- frontend/src/contexts/productContext.jsx
+++ frontend/src/contexts/productContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import config from './config';
+import config from '@/config/config.js';
 export const ProductContext = createContext();
 
 export function ProductContextProvider({ children }) {
